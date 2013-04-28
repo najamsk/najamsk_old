334317642623cc4cab566f4b7ee458ae132e91f2
najam sikander awan <najam.sikander@gmail.com>
2013-04-28 13:00:53 +0500

    custom domain seems to work

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/najamsk/"
-http_images_path = "/najamsk/images"
-http_fonts_path = "/najamsk/fonts"
-css_dir = "public/najamsk/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
